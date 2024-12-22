ff90a4b20349769f1a77116523f0f230d358fa04
Mohammod Hanif <honifnoob@gmail.com>
2024-12-22 19:03:51 +0600

    cute ahh cat

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// async function getRandomDogImage() {
-//   const url = "https://random.dog/woof.json";
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   console.log(data);
-//   document.querySelector("#dog-img").src = data.url;
-// }
+async function getRandomCatImage() {
+  const url = "https://cataas.com/cat";
+  //   const res = await fetch(url);
+  //   const data = await res.json();
+  //   console.log(data);
+  document.querySelector("#cat-img").src = url;
+}
 function getRandomDogImage() {
   console.log(
     "This function should fetch a random dog image from the Dog CEO API"
